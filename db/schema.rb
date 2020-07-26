d8c0107f33b81ce5939edf6affcce84cbfec15d1
gelsy-0517 <toku.shigeru.5.17@gmail.com>
2020-07-26 12:59:27 +0900

    devise user management

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_112524) do
+ActiveRecord::Schema.define(version: 2020_07_22_162433) do
 
   create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -26,8 +26,13 @@ ActiveRecord::Schema.define(version: 2020_07_05_112524) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name", "email"], name: "index_users_on_name_and_email"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "scores", "users"
